fb7d140cfecbd1a66891b71fd3840a459b8157d4
Brock <brockwrob@gmail.com>
2020-01-08 15:31:56 -0600

    start

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,30 @@
 import React from "react"
+import 'typeface-roboto';
+import AppBar from '@material-ui/core/AppBar'
+import CssBaseline from '@material-ui/core/CssBaseline'
+import {
+  Button,
+  CardActionArea,
+  Fab,
+  IconButton,
+  Link,
+} from "gatsby-theme-material-ui";
 
-export default () => <div>Hello world!</div>
+export default () => <App />
+
+class App extends React.Component {
+  render() {
+    return (
+      <React.Fragment>
+        <CssBaseline />
+        <AppBar>
+          App text
+        </AppBar>
+        <Button>
+          Button Text
+        </Button>
+        text
+      </React.Fragment>
+    )
+  }
+}
