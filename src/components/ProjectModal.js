377bf7b0d8af6df115a4fc0feab38442f52739fe
Brock <brockwrob@gmail.com>
2020-01-10 17:03:21 -0600

    converted project modal to card

--- src/components/ProjectModal.js
+++ src/components/ProjectModal.js
@@ -1,5 +1,9 @@
 import React from "react"
 import 'typeface-roboto';
+import Card from '@material-ui/core/Card'
+import CardHeader from '@material-ui/core/CardHeader'
+import CardContent from '@material-ui/core/CardContent'
+import CardMedia from '@material-ui/core/CardMedia'
 import Grid from '@material-ui/core/Grid'
 import Modal from '@material-ui/core/Modal'
 import Paper from '@material-ui/core/Paper'
@@ -12,21 +16,28 @@ import {
   Link,
 } from "gatsby-theme-material-ui";
 
+import CloseIcon from '@material-ui/icons/Close';
+
 export default class ProjectModal extends React.Component {
   render() {
     return this.props.project ? (
       <Modal open={this.props.open} onClose={this.props.onClose}>
         <Grid container style={{height: '100%'}} justify='center' alignItems='center'>
-          <Grid item xs={6} style={{height: '100%', overflow: 'auto'}}>
-            <Paper>
-              <img src={this.props.project.img} style={{width: '100%'}} />
-              <Typography variant='h1'>
-                {this.props.project.title}
-              </Typography>
-              <Typography variant='body'>
-                {this.props.project.desc}
-              </Typography>
-            </Paper>
+          <Grid item xs={6}>
+            <Card>
+              <CardMedia style={{height: 0, paddingTop: '56%'}} image={this.props.project.img} />
+              <CardHeader title={this.props.project.title} action={
+                  <IconButton aria-label='close' onClick={this.props.onClose}>
+                    <CloseIcon />
+                  </IconButton>
+                }
+              />
+              <CardContent>
+                <Typography variant='body1'>
+                  {this.props.project.desc}
+                </Typography>
+              </CardContent>
+            </Card>
           </Grid>
         </Grid>
       </Modal>
