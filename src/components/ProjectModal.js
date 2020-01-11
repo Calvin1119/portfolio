830f3fe8f216408f44378c175a9b25c18783eebe
Brock <brockwrob@gmail.com>
2020-01-11 09:24:53 -0600

    added an image carousel for projects

--- src/components/ProjectModal.js
+++ src/components/ProjectModal.js
@@ -1,6 +1,7 @@
 import React from "react"
 import 'typeface-roboto';
 import Card from '@material-ui/core/Card'
+import CardActions from '@material-ui/core/CardActions'
 import CardHeader from '@material-ui/core/CardHeader'
 import CardContent from '@material-ui/core/CardContent'
 import CardMedia from '@material-ui/core/CardMedia'
@@ -16,7 +17,9 @@ import {
   Link,
 } from "gatsby-theme-material-ui";
 
+import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
 import CloseIcon from '@material-ui/icons/Close';
+import ChevronRightIcon from '@material-ui/icons/ChevronRight';
 
 export default class ProjectModal extends React.Component {
   render() {
@@ -25,7 +28,7 @@ export default class ProjectModal extends React.Component {
         <Grid container style={{height: '100%'}} justify='center' alignItems='center'>
           <Grid item xs={6}>
             <Card>
-              <CardMedia style={{height: 0, paddingTop: '56%'}} image={this.props.project.img} />
+              <Carousel images={this.props.project.img}/>
               <CardHeader title={this.props.project.title} action={
                   <IconButton aria-label='close' onClick={this.props.onClose}>
                     <CloseIcon />
@@ -37,10 +40,52 @@ export default class ProjectModal extends React.Component {
                   {this.props.project.desc}
                 </Typography>
               </CardContent>
+              <CardActionArea>
+                <CardActions>
+                  <Button href={this.props.project.link}>
+                    Demo
+                  </Button>
+                  <Button href={this.props.project.link}>
+                    Source
+                  </Button>
+                </CardActions>
+              </CardActionArea>
             </Card>
           </Grid>
         </Grid>
       </Modal>
     ) : null
+  }
+}
+
+class Carousel extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      curr: 0
+    }
+    this.left = this.left.bind(this)
+    this.right=  this.right.bind(this)
+  }
+  left() {
+    this.setState({curr: this.state.curr - 1})
+  }
+  right() {
+    this.setState({curr: this.state.curr + 1})
+  }
+  render() {
+    return !Array.isArray(this.props.images) ? (
+      <CardMedia style={{height: 0, paddingTop: '56%'}} image={this.props.images} />
+    ) : (
+      <div style={{position: 'relative'}}>
+        <CardMedia style={{height: 0, paddingTop: '56%'}} image={this.props.images[this.state.curr]} />
+        <IconButton style={{position: 'absolute', top: '50%', left: 0, transform: 'translateY(-50%)'}} onClick={this.left} disabled={this.state.curr == 0}>
+          <ChevronLeftIcon />
+        </IconButton>
+        <IconButton style={{position: 'absolute', top: '50%', right: 0, transform: 'translateY(-50%)'}} onClick={this.right} disabled={this.state.curr == this.props.images.length - 1}>
+          <ChevronRightIcon />
+        </IconButton>
+      </div>
+    )
   }
 }
--- src/pages/projects.js
+++ src/pages/projects.js
@@ -20,19 +20,19 @@ const projects = [
   {
     title: 'p1',
     desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer a tellus orci. Donec nisi mi, posuere a dui vitae, rhoncus luctus velit. Etiam interdum laoreet est, eu fermentum purus scelerisque sed. Proin ac erat sollicitudin quam scelerisque imperdiet non ut diam. Nullam at eros ex. Curabitur id dignissim diam. Quisque vel congue nunc, sed mattis mi. Curabitur consectetur pretium condimentum. Sed varius consequat dui, at iaculis nunc pretium a. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin tempus lorem leo, vestibulum pellentesque felis congue in. Quisque nec elit sit amet orci vulputate sagittis.',
-    img: image,
-    link: 'google.com',
+    img: [image, image, image],
+    link: 'http://www.google.com',
   },
   {
     title: 'p2',
     desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer a tellus orci. Donec nisi mi, posuere a dui vitae, rhoncus luctus velit. Etiam interdum laoreet est, eu fermentum purus scelerisque sed. Proin ac erat sollicitudin quam scelerisque imperdiet non ut diam. Nullam at eros ex. Curabitur id dignissim diam. Quisque vel congue nunc, sed mattis mi. Curabitur consectetur pretium condimentum. Sed varius consequat dui, at iaculis nunc pretium a. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin tempus lorem leo, vestibulum pellentesque felis congue in. Quisque nec elit sit amet orci vulputate sagittis.',
-    img: image,
+    img: [image, image, image],
     link: 'google.com',
   },
   {
     title: 'p3',
     desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer a tellus orci. Donec nisi mi, posuere a dui vitae, rhoncus luctus velit. Etiam interdum laoreet est, eu fermentum purus scelerisque sed. Proin ac erat sollicitudin quam scelerisque imperdiet non ut diam. Nullam at eros ex. Curabitur id dignissim diam. Quisque vel congue nunc, sed mattis mi. Curabitur consectetur pretium condimentum. Sed varius consequat dui, at iaculis nunc pretium a. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin tempus lorem leo, vestibulum pellentesque felis congue in. Quisque nec elit sit amet orci vulputate sagittis.',
-    img: image,
+    img: [image, image, image],
     link: 'google.com',
   },
 ]
@@ -58,7 +58,7 @@ export default class About extends React.Component {
     for (let i = 0; i < projects.length; i++) {
       tiles.push(
         <GridListTile key={i}>
-          <img src={projects[i].img} />
+          <img src={projects[i].img[0]} />
           <GridListTileBar
             title={projects[i].title}
             subtitle={projects[i].desc}
